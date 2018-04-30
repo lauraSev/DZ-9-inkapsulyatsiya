b3673542b5614c66c508295c456ce7449c58cfd7
laura <severyuhina.lv@eksmo.ru>
2018-05-01 00:19:14 +0300

    DZ9-dorabotka-2

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ class Car
     public $color = '';
     public $toplivo = '';
     public $doors = '';
+	private $status = 'stop';
 
     public function __construct($color, $toplivo, $doors)
     {
@@ -16,15 +17,19 @@ class Car
 
     public function go()
     {
+		$this->status = 'edet';
     }
 
     public function stop()
     {
+		$this->status = 'stop';
     }
 }
 
 $car1 = new Car ('Красный', 'Дизель', '4 двери');
 $car2 = new Car('Синий', 'Бензин', '2 двери');
+$car1->go();
+$car2->stop();
 print_r($car1);
 print_r($car2);
 
@@ -39,8 +44,9 @@ class TV
         $this->diagonal = $diagonal;
     }
 
-    public function ChangeChanel()
+    public function ChangeChanel( $chanal)
     {
+		$this->chanal = $chanal;
     }
 
     public function OnOff()
@@ -51,6 +57,7 @@ class TV
 
 $TV1 = new TV('24');
 $TV1->OnOff();
+$TV1->ChangeChanel('14');
 $TV2 = new TV('32');
 print_r($TV1);
 print_r($TV2);
@@ -60,6 +67,7 @@ class Pen
     public $color = '';
     public $price = '';
     public $dlina = '';
+	private $pishet = 'da';
 
     public function __construct($color, $price, $dlina)
     {
@@ -68,13 +76,16 @@ class Pen
         $this->dlina = $dlina;
     }
 
-    public function pishet()
+    public function pishet($pishet)
     {
+		 $this->pishet = $pishet;
     }
 }
 
 $pen1 = new Pen ('синяя', '12р', '12см');
 $pen2 = new Pen ('красная', '14р', '16см');
+$pen1->pishet ('da');
+$pen2->pishet ('net');
 print_r($pen1);
 print_r($pen2);
 
@@ -83,6 +94,7 @@ class Duck
     public $tsvet = '';
     public $vozrast = 0;
     public $poroda = '';
+	private $mojetletat ='';
 
     public function __construct($tsvet, $vozrast, $poroda)
     {
@@ -91,13 +103,16 @@ class Duck
         $this->poroda = $poroda;
     }
 
-    public function fly()
+    public function fly($mojetletat)
     {
+		$this->mojetletat = $mojetletat;
     }
 }
 
 $duck1 = new Duck ('пестрая', 6, 'дикая');
 $duck2 = new Duck ('белая', 8, 'домашняя');
+$duck1->fly('mojet letat');
+$duck2->fly('ne letaet');
 print_r($duck1);
 print_r($duck2);
 
